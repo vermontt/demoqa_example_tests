4ef42061ece3f0aa1e4f1ce1ee9c7d88991684b5
vermontt <lazar.evilgenius@yandex.ru>
2023-04-08 15:07:22 +0300

    add selenoid

--- src/test/java/com/lazarev/tests/BaseTest.java
+++ src/test/java/com/lazarev/tests/BaseTest.java
@@ -10,7 +10,8 @@ public class BaseTest {
 
     @BeforeEach
     void setUp() {
-        Configuration.startMaximized = true;
+        Configuration.browserSize = "1920x1080";
         Configuration.baseUrl = "https://demoqa.com";
+        Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub/";
     }
 }
